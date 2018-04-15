98c86f3a95d7953f83dcd1ca6d5b456a14168184
entalent <1512503696@qq.com>
2018-04-15 15:00:49 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -200,7 +200,7 @@ def impute_data_mice(data: pd.DataFrame):
 
 
 def main():
-    for s in ['column']:
+    for s in ['mode', 'column', 'knn', ]:
         # read_dataset(u"E:\\BaiduNetdiskDownload\作业1数据集\\NFL Play by Play 2009-2017 (v4).csv\\NFL Play by Play 2009-2017 (v4).csv",
         #              '1.csv', "NFL", s)
         read_dataset(u"E:\\BaiduNetdiskDownload\\作业1数据集\\Building_Permits.csv\\Building_Permits.csv",
